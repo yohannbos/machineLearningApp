0629edeebdba048361187387927de67fef5b8bb2
Yohann Bosco <yohann.bosco@gmail.com>
2017-05-12 17:06:43 +0200

    Affiche une liste de clients

--- machineLearningApp/__main__.py
+++ machineLearningApp/__main__.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    customers = ["Hafid Traikzi","Yohann Bosco", "Carole Gombauld", "Yannick François"]
+    return render_template('home.html', customers=customers)
 
 if __name__ == "__main__":
     app.run()
