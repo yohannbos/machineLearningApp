5ddb0aa12873b8e37cf39100dbc0bd6c3034c289
Yohann Bosco <yohann.bosco@gmail.com>
2017-05-12 15:56:01 +0200

    Range le code dans un module
    Utilise
    
    Mise en place de l'exemple de Flask

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='machineLearningApp',
+    version='1.0',
+    description='Un truc pour apprendre le Machine Learning',
+    author='Yohann Bosco, Carole Gombauld et Hafid Traikzi',
+    author_email='yohann.bosco@gmail.com',
+    packages=['machineLearningApp'],
+    install_requires=['flask'],
+)
+
